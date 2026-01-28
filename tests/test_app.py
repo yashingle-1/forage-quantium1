857e098db0074d48a1b404c5f577d1b9044e1656
yashingle <yashinglelive@gmail.com>
2026-01-28 12:47:33 +0000

    Created a Dash Testing Framework

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,6 @@
 from dash import dcc, html
 from app import app
 
-
 def find_component(component_type, layout):
     """Recursively search layout for a component type"""
     found = []
@@ -15,21 +14,14 @@ def find_component(component_type, layout):
             found.extend(find_component(component_type, layout.children))
     return found
 
-
 def test_header_present():
     headers = find_component(html.H1, app.layout)
     assert len(headers) > 0
-
 
 def test_visualisation_present():
     graphs = find_component(dcc.Graph, app.layout)
     assert len(graphs) > 0
 
-
 def test_region_picker_present():
     radios = find_component(dcc.RadioItems, app.layout)
     assert len(radios) > 0
-
-
-#Run command 
-#pytest
